14a0401c8b346d12e12ea22d9ffae5e8b7f3e66c
Guy Avitan <guyya23@gmail.com>
2026-01-16 00:50:46 +0200

    Drop-down menu Default option Change

--- app/[locale]/booking/page.tsx
+++ app/[locale]/booking/page.tsx
@@ -49,9 +49,9 @@ export default function BookingPage() {
             <div>
               <label htmlFor="service" className="block text-sm font-medium text-gray-700 dark:text-gray-300">{t('selectService')}</label>
               <select id="service" name="service" required className="block w-full px-3 py-2 mt-1 text-gray-900 bg-gray-200 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <option>{t('services.haircutBeardTrim')}</option>
                 <option>{t('services.haircut')}</option>
                 <option>{t('services.beardTrim')}</option>
-                <option>{t('services.haircutBeardTrim')}</option>
               </select>
             </div>
             <div>
